ec717b7656737008a17ca222802f04616e492ba1
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-03-05 05:29:51 +0530

    Update print.ps1

--- Weapons/print.ps1
+++ Weapons/print.ps1
@@ -1,3 +1,5 @@
+# Weapon-05
+
 "You have won 100% free coupons for lifetime ..." > C:\ProgramData\.temp\freecoupon
 "Have Fun !!" >> C:\ProgramData\.temp\freecoupon
 notepad.exe C:\ProgramData\.temp\freecoupon
